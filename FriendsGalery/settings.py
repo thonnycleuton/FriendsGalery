1b48a299f5e7f522392357174a2b0647c7b62dfd
thonnycleuton <thonnycleuton@gmail.com>
2021-02-12 00:14:55 -0300

    preparing to deploy

--- FriendsGalery/settings.py
+++ FriendsGalery/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -137,3 +138,5 @@ THUMBNAIL_ALIASES = {
         'avatar': {'size': (50, 50), 'crop': True},
     },
 }
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
